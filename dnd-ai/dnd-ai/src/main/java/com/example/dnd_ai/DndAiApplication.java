60d190db578254fda3aa72632efd4e79d37bf8cb
batfiowoof <bojidarwee@gmail.com>
2026-03-02 16:40:55 +0200

    initial commit

---
+++ dnd-ai/dnd-ai/src/main/java/com/example/dnd_ai/DndAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnd_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndAiApplication.class, args);
+	}
+
+}
---
+++ dnd-ai/dnd-ai/src/test/java/com/example/dnd_ai/DndAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dnd_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
